487cef03ac5199928961b1aeaaafe154c1e91d58
Jagan Teki <jagan@edgeble.ai>
2023-12-13 11:41:34 +0530

    scripts: Drop few packages to uninstall
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- scripts/apt-uninstall.sh
+++ scripts/apt-uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-apt-get remove --purge make build-essential libmtd-dev liblua5.4-dev libubi-dev libubootenv-dev libubootenv-tool libfdisk-dev dh-lua git -y
+apt-get remove --purge make build-essential git -y
 apt-get autoremove -y
 apt-get clean
